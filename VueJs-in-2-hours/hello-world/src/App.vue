8cec5d74141d935b2f094c34deee4909dcdd091b
trungpd1102 <letsmile95@gmail.com>
2021-06-26 07:52:15 +0700

    Update

--- VueJs-in-2-hours/hello-world/src/App.vue
+++ VueJs-in-2-hours/hello-world/src/App.vue
@@ -1,28 +1,94 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-show="isShow">
+      <p>Hello VueJs</p>
+    </div>
+    <button @click="isShow = !isShow">Show/Hidden</button>
+    <!-- <h1 v-bind:id="vID">Duc Trung Vue</h1> -->
+    <h1 :id="ID">Duc Trung Vue</h1>
+
+    <!-- id binding -->
+    <input type="text" :disabled="inputDisabled" />
+    <button @click="inputDisabled = !inputDisabled">
+      Disable/Enable Input
+    </button>
+
+    <!-- class binding -->
+    <h1 :class="{ isSelected: selected }">Class Binding</h1>
+    <button @click="selected = !selected">Change color</button>
+
+    <div>
+      <input type="text" v-model="ownerName[0]" />
+      <p>{{ ownerName[0] }}</p>
+    </div>
+
+    <div>
+      <input type="text" v-model="tech.vueJs" />
+      <p>{{ tech.vueJs }}</p>
+    </div>
+
+    <!-- Event handler -->
+    <div>
+      <input type="text" v-model="tech.reactJs" />
+      <p>{{ tech.reactJs }}</p>
+      <button v-on:click="tech.reactJs = 'This is reactJs'">
+        Change input
+      </button>
+      <button v-on:mouseover="tech.reactJs = 'This is reactJs'">
+        Mouse Over
+      </button>
+    </div>
+
+    <!-- preventDefault event -->
+    <div>
+      <form action="/prevent">
+        <input type="text" v-model="tech.reactJs" />
+        <p>{{ tech.reactJs }}</p>
+        <button type="submit" @click.prevent="tech.reactJs = 'This is reactJs'">
+          Change input
+        </button>
+      </form>
+    </div>
+
+    <!-- Confition render -->
+    <div>
+      <button @click="isDisplay = !isDisplay">
+        <span v-if="isDisplay">Hide</span>
+        <span v-else>Show</span>
+      </button>
+      <div v-if="isDisplay">Duc Trung Hachi-X</div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Menu from "./components/Menu.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      isShow: true,
+      ID: "v-id",
+      inputDisabled: false,
+      selected: true,
+      ownerName: ["Pham Duc Trung", "Vu Thi Ngoc"],
+      tech: {
+        reactJs: "React Js",
+        angular: "Angular",
+        vueJs: "VueJs",
+      },
+      isDisplay: true,
+    };
+  },
+  component: {
+    Menu,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.isSelected {
+  color: tomato;
 }
 </style>
---
+++ VueJs-in-2-hours/hello-world/src/components/Menu.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <input type="text" v-model="name" />
+    <input type="checkbox" v-model="accept" /> Check
+    <div>{{ name }}</div>
+    <div>{{ accept }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Menu",
+  data() {
+    return {
+      name: "Pham Duc Trung",
+      accept: true,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
